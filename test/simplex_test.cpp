e0ca5221f5502a60b7919755e031afc7d01c9c24
Paul Swoboda <swoboda@math.uni-heidelberg.de>
2016-07-27 08:38:43 +0200

    CATCH test format with one main function only

--- test/simplex_test.cpp
+++ test/simplex_test.cpp
@@ -1,5 +1,3 @@
-#define CATCH_CONFIG_MAIN 
-#include "catch.hpp"
 #include <vector>
 #include "factors/simplex_factor.hxx"
 
---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#define CATCH_CONFIG_MAIN 
+#include "catch.hpp"
+
+// include all tests
+#include "simplex_test.cpp"
