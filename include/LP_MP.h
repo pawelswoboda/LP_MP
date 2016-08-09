4977cfef1b140db15359eed9455733c970b9da57
Jan Kuske <jkuske86@gmail.com>
2016-08-09 15:22:09 +0200

    made primalsolutionstorage as a single file

--- include/LP_MP.h
+++ include/LP_MP.h
@@ -17,6 +17,7 @@
 #include "topological_sort.hxx"
 #include <memory>
 #include <iterator>
+#include "primal_solution_storage.hxx"
 #include "lp_interface/lp_interface.h"
 
 namespace LP_MP {
@@ -24,34 +25,6 @@ namespace LP_MP {
 // forward declaration
 class MessageTypeAdapter;
 class MessageIterator;
-
-// do zrobienia: better use some custom vector with packing. Essentially only 3 values are needed: true, false, unknown, which can be stored with 2 bits. Also using 3 bits for two values is possible, but runtime cost may be too high with such a difficult construction. Alternatively, pack 2 entries into 3 bits and then use chunk of, say, 64 bits, such that every k entries we have alignment.
-static constexpr unsigned char unknownState = 3;
-class PrimalSolutionStorage : public std::vector<unsigned char> {
-public:
-   using Element = std::vector<unsigned char>::iterator;
-
-   PrimalSolutionStorage() {}
-   template<typename FACTOR_ITERATOR>
-   PrimalSolutionStorage(FACTOR_ITERATOR factorIt, FACTOR_ITERATOR factorItEnd)
-   {
-      INDEX size = 0;
-      for(;factorIt != factorItEnd; ++factorIt) {
-         size += (*factorIt)->PrimalSize();
-      }
-      this->resize(size,unknownState);
-      //this->shrink_to_fit();
-      //std::cout << "primal size = " << this->size() << "\n";
-   }
-
-   void Initialize()
-   {
-      std::fill(this->begin(), this->end(), unknownState);
-   }
-
-};  
-
-
 
 // pure virtual base class for factor container used by LP class
 class FactorTypeAdapter
