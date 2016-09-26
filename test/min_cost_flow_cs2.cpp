99dede7115eb7441b0c5c5832dc1614ba12defdb
Paul Swoboda <swoboda@math.uni-heidelberg.de>
2016-09-26 08:16:51 +0200

    added minimum cost flow test skeleton

---
+++ test/min_cost_flow_cs2.cpp
@@ -0,0 +1,11 @@
+#include "catch.hpp"
+#include "LP_MP.h"
+#include <vector>
+#include "factors/min_cost_flow_factor_cs2.hxx"
+
+TEST_CASE("CS2" "[CS2 minimum cost flow factor]") {
+   // build 3x3 assignment problem and check whether MaximallyPerturbCosts gives an admissible update
+
+   
+}
+
