5682f7077b4b602d91a6fcb820c433eb11706f95
Jan Kuske <jkuske86@gmail.com>
2016-10-05 15:48:33 +0200

    LinExpr += operator only works with REAL or int

--- solvers/discrete_tomography/discrete_tomography_localsolver.cpp
+++ solvers/discrete_tomography/discrete_tomography_localsolver.cpp
@@ -2,5 +2,5 @@
 #include "lp_interface/lp_localsolver.hxx"
 #include "visitors/standard_visitor.hxx"
 using namespace LP_MP;
-LP_MP_CONSTRUCT_SOLVER_WITH_INPUT_AND_LPVISITOR(FMC_DT, DiscreteTomographyTextInput::ParseProblem, StandardTighteningVisitor, LpInterfaceLocalSolver);
-
+//LP_MP_CONSTRUCT_SOLVER_WITH_INPUT_AND_LPVISITOR(FMC_DT, DiscreteTomographyTextInput::ParseProblem, StandardTighteningVisitor, LpInterfaceLocalSolver);
+LP_MP_CONSTRUCT_SOLVER_WITH_INPUT_AND_LPVISITOR(FMC_DT, DiscreteTomographyTextInput::ParseProblem<FMC_DT>, StandardTighteningVisitor, LpInterfaceLocalSolver);
