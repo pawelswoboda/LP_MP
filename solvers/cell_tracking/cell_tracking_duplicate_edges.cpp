3062073ba161487a1a11d7c74586327005d6744a
Paul SWOBODA <paul.swoboda@ist.ac.at>
2017-06-23 15:03:40 +0200

    Added cell tracking with duplicated edges executable.

---
+++ solvers/cell_tracking/cell_tracking_duplicate_edges.cpp
@@ -0,0 +1,12 @@
+
+#include "cell_tracking.h"
+#include "visitors/standard_visitor.hxx"
+using namespace LP_MP;
+int main(int argc, char* argv[])
+
+{
+Solver<FMC_CELL_TRACKING_DUPLICATE_EDGES,LP,StandardTighteningVisitor> solver(argc,argv);
+solver.ReadProblem(cell_tracking_parser_2d::ParseProblem<Solver<FMC_CELL_TRACKING_DUPLICATE_EDGES,LP,StandardTighteningVisitor>>);
+return solver.Solve();
+
+}
